7a84fa4c0d4fac35d9c2cea0cd675e3b16e0b140
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-14 16:12:18 +0100

    Ruby code to HTML template

--- week_3/sinatra-test/hello.rb
+++ week_3/sinatra-test/hello.rb
@@ -1,7 +1,10 @@
-require 'sinatra'
+# require 'sinatra'
+#
+# get '/' do
+#   @name = %w(Amigo Oscar Viking).sample
+#   erb :index
+# end
 
-get '/' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
-end
+array = %w(Amigo Oscar Viking)
+
+print array 
